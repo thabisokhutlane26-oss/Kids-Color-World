9b94c04f6460ee90d11ad5607a8e4aabedc4f388
thabisokhutlane26-oss <thabisokhutlane26@gmail.com>
2026-09-23 17:07:07 +0200

    Implement MainActivity with coloring functionality

---
+++ app/src/main/java/com/kidscolorworld/app/MainActivity.java
@@ -0,0 +1,81 @@
+package com.kidscolorworld.app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView coloringPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        coloringPage = findViewById(R.id.coloringPage);
+
+        setupColorButtons();
+
+        Button clearButton = findViewById(R.id.clearButton);
+
+        clearButton.setOnClickListener(v -> {
+            coloringPage.setImageResource(R.drawable.coloring_page);
+
+            Toast.makeText(
+                    MainActivity.this,
+                    "Picture cleared!",
+                    Toast.LENGTH_SHORT
+            ).show();
+        });
+    }
+
+    private void setupColorButtons() {
+
+        int[] colors = {
+                Color.RED,
+                Color.rgb(255, 152, 0),
+                Color.YELLOW,
+                Color.GREEN,
+                Color.BLUE,
+                Color.rgb(156, 39, 176),
+                Color.rgb(255, 105, 180),
+                Color.BLACK
+        };
+
+        int[] buttonIds = {
+                R.id.colorRed,
+                R.id.colorOrange,
+                R.id.colorYellow,
+                R.id.colorGreen,
+                R.id.colorBlue,
+                R.id.colorPurple,
+                R.id.colorPink,
+                R.id.colorBlack
+        };
+
+        for (int i = 0; i < buttonIds.length; i++) {
+
+            ImageButton button = findViewById(buttonIds[i]);
+
+            final int color = colors[i];
+
+            button.setBackgroundColor(color);
+
+            button.setOnClickListener(v -> {
+
+                Toast.makeText(
+                        MainActivity.this,
+                        "Color selected!",
+                        Toast.LENGTH_SHORT
+                ).show();
+            });
+        }
+    }
+}
