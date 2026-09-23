79e23d89894220381935415ef7cee52a8b022042
thabisokhutlane26-oss <thabisokhutlane26@gmail.com>
2026-09-23 17:35:30 +0200

    Change MainActivity to extend Activity instead of AppCompatActivity

--- app/src/main/java/com/kidscolorworld/app/MainActivity.java
+++ app/src/main/java/com/kidscolorworld/app/MainActivity.java
@@ -1,16 +1,14 @@
 package com.kidscolorworld.app;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private ImageView coloringPage;
 
